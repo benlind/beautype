7fc583bf49f788f06d72a6b779fd121727fea9be
Ben Lind <bendlind@gmail.com>
2017-07-02 11:41:51 -0400

    Show warning when closing modified page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+    var editor = $('textarea');
+    var isDirty = false;  // has the textbox been modified?
+
+    handleDirt();
+
     $('#change-theme').click(function(e) {
         if ($('body').hasClass('theme-dark')) {
             $('body').removeClass('theme-dark').addClass('theme-light');
@@ -8,5 +13,14 @@ $(document).ready(function() {
             $('body').removeClass('theme-light').addClass('theme-dark');
         }
     });
+
+    // Show a "really close?" message before exit
+    function handleDirt() {
+        editor.on('input', function() { isDirty = true });
+
+        window.onbeforeunload = function() {
+            if (isDirty) return 'Close without saving?';
+        };
+    }
 
 });
